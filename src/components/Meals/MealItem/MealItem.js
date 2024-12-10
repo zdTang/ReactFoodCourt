b71148495c155f594a3ab15c6555662de5bca6d9
Zhendong Tang <44347399+zdTang@users.noreply.github.com>
2024-12-10 16:46:50 -0500

    Insert a Image and add animation style to the Image

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,21 +1,25 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
-import CartContext from '../../../store/cart-context';
+import MealItemForm from "./MealItemForm";
+import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
 
   const price = `$${props.price.toFixed(2)}`;
 
-  const addToCartHandler = amount => {
+  const addToCartHandler = (amount) => {
     cartCtx.addItem({
       id: props.id,
       name: props.name,
       amount: amount,
-      price: props.price
+      price: props.price,
     });
+  };
+
+  const clickImage = () => {
+    console.log(props.name);
   };
 
   return (
@@ -24,6 +28,16 @@ const MealItem = (props) => {
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <img
+          className={classes.pressableImage}
+          onClick={clickImage}
+          src="/1.jpg"
+          alt="Girl in a jacket"
+          width="100"
+          height="60"
+        />
       </div>
       <div>
         <MealItemForm onAddToCart={addToCartHandler} />
